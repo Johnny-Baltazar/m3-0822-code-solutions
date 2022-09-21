b370e5f88dcf16158a6bf062e154900d67606593
Johnny Baltazar <baltazarjohnny@yahoo.com>
2022-09-20 19:53:18 -0700

    Delete commented out code

--- fetch-in-react/src/app.jsx
+++ fetch-in-react/src/app.jsx
@@ -12,13 +12,16 @@ class App extends React.Component {
 
   componentDidMount() {
     /* your code here */
-    this.setState({ isLoading: false });
+    this.setState({ });
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => {
         return res.json();
       })
       .then(users => {
-        this.setState({ users });
+        this.setState({
+          users,
+          isLoading: false
+        });
       })
       .catch(err => console.error('Fetch failed!', err));
   }
